561d945382f2bb38c929d69f13ba74d593d241a6
Ted <washawgolla@gmail.com>
2017-10-14 18:35:32 -0400

    get todos by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 let app = express();
 
 const {mongoose} = require('./db/mongoose');
@@ -28,6 +30,25 @@ app.get('/todos', (req, res) => {
         res.status(400).body(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    
+    todo.findOne({_id : req.params.id}).then((todo) => {
+            if (!todo) {
+                res.status(404).send();
+            } 
+            
+            res.send({todo});
+
+        }).catch((err) => {
+            res.status(400).send();
+        });
+    }    
+);
 
 app.listen(3000, () => {
     console.log('app listing at port 3000');
